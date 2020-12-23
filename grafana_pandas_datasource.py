cbb3459812a0eb34a2a092f372610bc40bb35ba0
Andreas Motl <andreas.motl@panodata.org>
2020-12-23 23:45:12 +0100

    Adjust license texts once more

--- grafana_pandas_datasource.py
+++ grafana_pandas_datasource.py
@@ -1,11 +1,25 @@
 """
+Copyright 2017 Linar <linar@jether-energy.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+"""
 A REST API based on Flask for serving Pandas Dataframes to Grafana.
 
 The idea is to use ``flask_restful`` and create a Blueprint to be
 used by a parent project (i.e. a larger API project where ``/grafana/``
 endpoints are used by Grafana's SimpleJson plugin).
 """
-
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS, cross_origin
 import pandas as pd
