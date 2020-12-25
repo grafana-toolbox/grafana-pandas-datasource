1c61723e3b80a6ed2fcd3d108a2260e43c1ce358
Andreas Motl <andreas.motl@panodata.org>
2020-12-25 15:57:11 +0100

    Slightly improve sinewave-midnights demo program

--- examples/sinewave-midnights/demo.py
+++ examples/sinewave-midnights/demo.py
@@ -7,23 +7,24 @@ To query the reader, use ``<reader_name>:<query_string>``, e.g.
 """
 import pandas as pd
 import numpy as np
-from grafana_pandas_datasource.core import get_application, add_annotation_reader, add_reader
+from grafana_pandas_datasource.core import get_application, add_reader, add_annotation_reader
 
 
 def main():
-
-    # Sample annotation reader.
-    add_annotation_reader(
-        'midnights',
-        lambda query_string, ts_range:
-            pd.Series(index=pd.date_range(ts_range['$gt'], ts_range['$lte'], freq='D', normalize=True), dtype='float64').fillna('Text for annotation - midnight'))
 
     # Sample timeseries reader.
     def get_sine(freq, ts_range):
         freq = int(freq)
         ts = pd.date_range(ts_range['$gt'], ts_range['$lte'], freq='H')
         return pd.Series(np.sin(np.arange(len(ts)) * np.pi * freq * 2 / float(len(ts))), index=ts).to_frame('value')
+
+    # Sample annotation reader.
+    def get_midnights(query_string, ts_range):
+        return pd.Series(index=pd.date_range(ts_range['$gt'], ts_range['$lte'], freq='D', normalize=True), dtype='float64').fillna('Text for annotation - midnight')
+
+    # Register data generation functions.
     add_reader('sine_wave', get_sine)
+    add_annotation_reader('midnights', get_midnights)
 
     # Get Flask application and invoke it.
     app = get_application()
