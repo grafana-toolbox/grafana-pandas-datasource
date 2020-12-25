b2ae560fd93bc0216aca76add348b4b2e8055dc8
Andreas Motl <andreas.motl@panodata.org>
2020-12-25 16:13:53 +0100

    Refactor utility functions into "util.py"

---
+++ grafana_pandas_datasource/util.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+from werkzeug.exceptions import abort
+
+
+def dataframe_to_response(target, df, freq=None):
+    response = []
+
+    if df.empty:
+        return response
+
+    if freq is not None:
+        orig_tz = df.index.tz
+        df = df.tz_convert('UTC').resample(rule=freq, label='right', closed='right').mean().tz_convert(orig_tz)
+
+    if isinstance(df, pd.Series):
+        response.append(_series_to_response(df, target))
+    elif isinstance(df, pd.DataFrame):
+        for col in df:
+            response.append(_series_to_response(df[col], target))
+    else:
+        abort(404, Exception('Received object is not a dataframe or series.'))
+
+    return response
+
+
+def dataframe_to_json_table(target, df):
+    response = []
+
+    if df.empty:
+        return response
+
+    if isinstance(df, pd.DataFrame):
+        response.append({'type': 'table',
+                         'columns': df.columns.map(lambda col: {"text": col}).tolist(),
+                         'rows': df.where(pd.notnull(df), None).values.tolist()})
+    else:
+        abort(404, Exception('Received object is not a dataframe.'))
+
+    return response
+
+
+def annotations_to_response(target, df):
+    response = []
+
+    # Single series with DatetimeIndex and values as text
+    if isinstance(df, pd.Series):
+        for timestamp, value in df.iteritems():
+            response.append({
+                "annotation": target, # The original annotation sent from Grafana.
+                "time": timestamp.value // 10 ** 6, # Time since UNIX Epoch in milliseconds. (required)
+                "title": value, # The title for the annotation tooltip. (required)
+                #"tags": tags, # Tags for the annotation. (optional)
+                #"text": text # Text for the annotation. (optional)
+            })
+    # Dataframe with annotation text/tags for each entry
+    elif isinstance(df, pd.DataFrame):
+        for timestamp, row in df.iterrows():
+            annotation = {
+                "annotation": target,  # The original annotation sent from Grafana.
+                "time": timestamp.value // 10 ** 6,  # Time since UNIX Epoch in milliseconds. (required)
+                "title": row.get('title', ''),  # The title for the annotation tooltip. (required)
+            }
+
+            if 'text' in row:
+                annotation['text'] = str(row.get('text'))
+            if 'tags' in row:
+                annotation['tags'] = str(row.get('tags'))
+
+            response.append(annotation)
+    else:
+        abort(404, Exception('Received object is not a dataframe or series.'))
+
+    return response
+
+
+def _series_to_annotations(df, target):
+    if df.empty:
+        return {'target': '%s' % (target),
+                'datapoints': []}
+
+    sorted_df = df.dropna().sort_index()
+    timestamps = (sorted_df.index.astype(pd.np.int64) // 10 ** 6).values.tolist()
+    values = sorted_df.values.tolist()
+
+    return {'target': '%s' % (df.name),
+            'datapoints': list(zip(values, timestamps))}
+
+
+def _series_to_response(df, target):
+    if df.empty:
+        return {'target': '%s' % (target),
+                'datapoints': []}
+
+    sorted_df = df.dropna().sort_index()
+
+    timestamps = (sorted_df.index.astype(np.int64) // 10 ** 6).values.tolist()
+
+    values = sorted_df.values.tolist()
+
+    return {'target': '%s' % (df.name),
+            'datapoints': list(zip(values, timestamps))}
